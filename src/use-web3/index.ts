bb81d485b9c51c3d5724805403326292d42a9cdf
Deftunk <martin.lavie.pro@gmail.com>
2022-01-03 11:05:18 +0000

    Deploying to gh-pages from master @ 2dc215b1d7a37c7d35f97c3dd27ccc7aef16729e 🚀

--- src/use-web3/index.ts
+++ src/use-web3/index.ts
@@ -5,13 +5,15 @@ import bbuxContractArtifacts from "./contracts/PengFam.json";
 
 //@ts-ignore
 const ETHEREUM = window.ethereum;
-const PENG_FAM_CONTRACT = "0x84218C9DBce1f453eDde52A69C7cE702fe62dFB8";
+// const PENG_FAM_CONTRACT = "0xa822793992F75dAfAB322672747D98019F7a8349";
+const PENG_FAM_CONTRACT = "0x393b4e7cDD7aA4B406f4e97876b3A84fAF04D6d8";
 
 export const useWeb3 = () => {
   const web3 = new Web3(ETHEREUM);
   if (ETHEREUM) {
     ETHEREUM.enable();
   }
+
   const pengFamilyContractERC720 = new web3.eth.Contract(
     bbuxContractArtifacts.abi as any,
     PENG_FAM_CONTRACT
